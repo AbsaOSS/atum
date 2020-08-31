e993f78919193753e02642410a43afaa7eaa9622
Daniel Kavan <dk1844@gmail.com>
2020-08-31 13:31:24 +0200

    scaladoc fix (reformat revert)

--- atum/src/main/scala/za/co/absa/atum/AtumImplicits.scala
+++ atum/src/main/scala/za/co/absa/atum/AtumImplicits.scala
@@ -27,25 +27,25 @@ import za.co.absa.atum.persistence.s3.{ControlMeasuresS3LoaderJsonFile, ControlM
 import scala.language.implicitConversions
 
 /**
- * The object contains implicit methods for Control Framework
- * Minimalistic example of enabling control measurements tracking:
- * {{{
- *   import za.co.absa.atum.Atum
- *   import za.co.absa.atum.AtumImplicits._
- *
- *   ...
- *
- *   spark.enableControlFrameworkTracking(sourceInfoFile = "/source/info/file/path")
- *
- *   ...
- *
- *   dataSet.setCheckpoint("Checkpoint Name")
- * }}}
- *
- * You can use enableControlFrameworkTracking() without parameters if the _INFO file
- * is in the path.
- *
- */
+  * The object contains implicit methods for Control Framework
+  * Minimalistic example of enabling control measurements tracking:
+  *   {{{
+  *   import za.co.absa.atum.Atum
+  *   import za.co.absa.atum.AtumImplicits._
+  *
+  *   ...
+  *
+  *   spark.enableControlFrameworkTracking(sourceInfoFile = "/source/info/file/path")
+  *
+  *   ...
+  *
+  *   dataSet.setCheckpoint("Checkpoint Name")
+  *   }}}
+  *
+  * You can use enableControlFrameworkTracking() without parameters if the _INFO file
+  * is in the path.
+  *
+  */
 object AtumImplicits {
   type DefaultControlInfoStorer = ControlMeasuresHdfsStorerJsonFile
   type DefaultControlInfoLoader = ControlMeasuresHdfsLoaderJsonFile
@@ -53,28 +53,28 @@ object AtumImplicits {
   implicit def StringToPath(path: String): Path = new Path(path)
 
   /**
-   * The class contains implicit methods for [[org.apache.spark.sql.SparkSession]].
-   */
+    * The class contains implicit methods for [[org.apache.spark.sql.SparkSession]].
+    */
   implicit class SparkSessionWrapper(sparkSession: SparkSession) {
 
     /**
-     * Enable control measurements tracking.
-     * Input and output info file names will be inferred automatically based on data source and destination paths
-     *
-     */
+      * Enable control measurements tracking.
+      * Input and output info file names will be inferred automatically based on data source and destination paths
+      *
+      */
     def enableControlMeasuresTracking(): SparkSession = {
       enableControlMeasuresTracking(None, None)
     }
 
     /**
-     * Enable control measurements tracking.
-     * Both input and output info file paths need to be provided
-     *
-     * Example info file path name: "data/input/wikidata.csv.info"
-     *
-     * @param sourceInfoFile      Pathname to a json-formatted info file containing control measurements
-     * @param destinationInfoFile Pathname to save the control measurement results to
-     */
+      * Enable control measurements tracking.
+      * Both input and output info file paths need to be provided
+      *
+      * Example info file path name: "data/input/wikidata.csv.info"
+      *
+      * @param sourceInfoFile      Pathname to a json-formatted info file containing control measurements
+      * @param destinationInfoFile Pathname to save the control measurement results to
+      */
     def enableControlMeasuresTracking(sourceInfoFile: String = "",
                                       destinationInfoFile: String = ""): SparkSession = {
       val hadoopConfiguration = sparkSession.sparkContext.hadoopConfiguration
@@ -107,13 +107,13 @@ object AtumImplicits {
     }
 
     /**
-     * Enable control measurements tracking.
-     * This is a generic way to enable control measurements tracking enabling to provide a custom
-     * control measurements loader and storer objects
-     *
-     * @param loader An object responsible for loading data source control measurements
-     * @param storer An object responsible for storing the result control measurements
-     */
+      * Enable control measurements tracking.
+      * This is a generic way to enable control measurements tracking enabling to provide a custom
+      * control measurements loader and storer objects
+      *
+      * @param loader An object responsible for loading data source control measurements
+      * @param storer An object responsible for storing the result control measurements
+      */
     def enableControlMeasuresTracking(loader: Option[ControlMeasuresLoader],
                                       storer: Option[ControlMeasuresStorer]): SparkSession =
       sparkSession.synchronized {
@@ -131,26 +131,26 @@ object AtumImplicits {
       }
 
     /**
-     * Explicitly disable control measurements tracking.
-     * After invoking this routine control measuress will not be tracked for the rest of the Spark Job
-     *
-     */
+      * Explicitly disable control measurements tracking.
+      * After invoking this routine control measuress will not be tracked for the rest of the Spark Job
+      *
+      */
     def disableControlMeasuresTracking(): SparkSession =
       sparkSession.synchronized {
         Atum.dispose(sparkSession)
 
         sparkSession
-      }
+    }
 
     /**
-     * Sets control measurements file name for the source and destination data set.
-     * The file name should not contain path as it will be inferred from data source and destination.
-     * Use this only if info file paths and not specified when calling enableControlFrameworkTracking()
-     *
-     * Example info file name: "_INFO"
-     *
-     * @param fileName A file name for control measurements info
-     */
+      * Sets control measurements file name for the source and destination data set.
+      * The file name should not contain path as it will be inferred from data source and destination.
+      * Use this only if info file paths and not specified when calling enableControlFrameworkTracking()
+      *
+      * Example info file name: "_INFO"
+      *
+      * @param fileName A file name for control measurements info
+      */
     def setControlMeasuresFileName(fileName: String): SparkSession = {
       setControlMeasuresInputFileName(fileName)
       setControlMeasuresOutputFileName(fileName)
@@ -158,59 +158,59 @@ object AtumImplicits {
     }
 
     /**
-     * Sets control measurements file name for the source data set.
-     * The file name should not contain path as it will be inferred from data source.
-     * Use this only if the input info file path and not specified when calling enableControlFrameworkTracking()
-     *
-     * Example info file name: "_INFO"
-     *
-     * @param fileName A file name for control measurements info
-     */
+      * Sets control measurements file name for the source data set.
+      * The file name should not contain path as it will be inferred from data source.
+      * Use this only if the input info file path and not specified when calling enableControlFrameworkTracking()
+      *
+      * Example info file name: "_INFO"
+      *
+      * @param fileName A file name for control measurements info
+      */
     def setControlMeasuresInputFileName(fileName: String): SparkSession = {
       Atum.setControlMeasuresInputFileName(fileName)
       sparkSession
     }
 
     /**
-     * Sets control measurements file name for the destination data set.
-     * The file name should not contain path as it will be inferred from data destination.
-     * Use this only if the output info file path and not specified when calling enableControlFrameworkTracking()
-     *
-     * Example info file name: "_INFO"
-     *
-     * @param fileName A file name for control measurements info
-     */
+      * Sets control measurements file name for the destination data set.
+      * The file name should not contain path as it will be inferred from data destination.
+      * Use this only if the output info file path and not specified when calling enableControlFrameworkTracking()
+      *
+      * Example info file name: "_INFO"
+      *
+      * @param fileName A file name for control measurements info
+      */
     def setControlMeasuresOutputFileName(fileName: String): SparkSession = {
       Atum.setControlMeasuresOutputFileName(fileName)
       sparkSession
     }
 
     /**
-     * The method sets workflow name for the current job
-     *
-     * @param workflowName Name of the checkpoint
-     */
+      * The method sets workflow name for the current job
+      *
+      * @param workflowName Name of the checkpoint
+      */
     def setControlMeasuresWorkflow(workflowName: String): SparkSession = {
       Atum.setWorkflowName(workflowName)
       sparkSession
     }
 
     /**
-     * Check if Control Framework is initialized
-     *
-     * @return true is Control Framework is initialized
-     */
+      * Check if Control Framework is initialized
+      *
+      * @return true is Control Framework is initialized
+      */
     def isControlMeasuresTrackingEnabled: Boolean = {
       sparkSession.sessionState.conf contains Constants.InitFlagKey
     }
 
     /**
-     * The method notifies Menas of a job failure
-     *
-     * @param jobStep          A job step name
-     * @param errorDescription An error description
-     * @param techDetails      A technical details
-     */
+      * The method notifies Menas of a job failure
+      *
+      * @param jobStep A job step name
+      * @param errorDescription An error description
+      * @param techDetails A technical details
+      */
     def setControlMeasurementError(jobStep: String, errorDescription: String, techDetails: String): SparkSession = {
       val errorDescriptionTrunc = if (errorDescription.length > Constants.maxErrorMessageSize)
         errorDescription.substring(0, Constants.maxErrorMessageSize)
@@ -229,16 +229,16 @@ object AtumImplicits {
   }
 
   /**
-   * The class contains implicit methods for [[org.apache.spark.sql.Dataset]].
-   */
+    * The class contains implicit methods for [[org.apache.spark.sql.Dataset]].
+    */
   implicit class DataSetWrapper(dataset: Dataset[Row]) {
     /**
-     * The method creates a new checkpoint by calculating control measurements of the dataset
-     * On first checkpoint Spark Session Key ControlFrameworkKeys.InfoFileVersionKey is updated
-     * to the info file stored version
-     *
-     * @param name Name of the checkpoint
-     */
+      * The method creates a new checkpoint by calculating control measurements of the dataset
+      * On first checkpoint Spark Session Key ControlFrameworkKeys.InfoFileVersionKey is updated
+      * to the info file stored version
+      *
+      * @param name Name of the checkpoint
+      */
     def setCheckpoint(name: String, persistInDatabase: Boolean = true): Dataset[Row] = {
       if (!(dataset.sparkSession.sessionState.conf contains Constants.InitFlagKey))
         throw new IllegalStateException("Control framework tracking is not initialized.")
@@ -252,12 +252,12 @@ object AtumImplicits {
     }
 
     /**
-     * The method returns the number of records in the dataframe calculated during the last checkpoint.
-     * If record count is absent in the checkpoint measurements, None is returned.
-     *
-     * This is useful to optimize out an additional df.count() invocation in a Spark job with
-     * enabled control measurements.
-     */
+      * The method returns the number of records in the dataframe calculated during the last checkpoint.
+      * If record count is absent in the checkpoint measurements, None is returned.
+      *
+      * This is useful to optimize out an additional df.count() invocation in a Spark job with
+      * enabled control measurements.
+      */
     def lastCheckpointRowCount: Option[Long] = {
       if (!(dataset.sparkSession.sessionState.conf contains Constants.InitFlagKey))
         throw new IllegalStateException("Control framework tracking is not initialized.")
@@ -271,11 +271,11 @@ object AtumImplicits {
     }
 
     /**
-     * The method registers a column rename of a column that is used for control measurements
-     *
-     * @param oldName A job step name
-     * @param newName An error description
-     */
+      * The method registers a column rename of a column that is used for control measurements
+      *
+      * @param oldName A job step name
+      * @param newName An error description
+      */
     def registerColumnRename(oldName: String, newName: String): Dataset[Row] = {
       if (!(dataset.sparkSession.sessionState.conf contains Constants.InitFlagKey))
         throw new IllegalStateException("Control framework tracking is not initialized.")
@@ -284,10 +284,10 @@ object AtumImplicits {
     }
 
     /**
-     * The method registers a column drop when it is no longer needed for the column to calculate control measurements
-     *
-     * @param columnName A column to be dropped from measurements
-     */
+      * The method registers a column drop when it is no longer needed for the column to calculate control measurements
+      *
+      * @param columnName A column to be dropped from measurements
+      */
     def registerColumnDrop(columnName: String): Dataset[Row] = {
       if (!(dataset.sparkSession.sessionState.conf contains Constants.InitFlagKey))
         throw new IllegalStateException("Control framework tracking is not initialized.")
@@ -296,20 +296,20 @@ object AtumImplicits {
     }
 
     /**
-     * The method fetches the initial control measurements and puts version from info file
-     * to ControlFrameworkKeys.InfoFileVersionKey Spark Session Key
-     *
-     */
+      * The method fetches the initial control measurements and puts version from info file
+      * to ControlFrameworkKeys.InfoFileVersionKey Spark Session Key
+      *
+      */
     def loadControlInfoFile(): Dataset[Row] = {
       Atum.controlFrameworkState.initializeControlInfo(dataset)
       dataset
     }
 
     /**
-     * The method saves the info file to the specified destination path on HDFS
-     *
-     * @param outputPath A directory or a file name to save the info file to.
-     */
+      * The method saves the info file to the specified destination path on HDFS
+      *
+      * @param outputPath A directory or a file name to save the info file to.
+      */
     def writeInfoFile(outputPath: String): Dataset[Row] = {
       Atum.controlFrameworkState.storeCurrentInfoFile(outputPath)
       dataset
