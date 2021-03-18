0efa209772de8cd32f0e70375af317ebcf295772
Sasa Zejnilovic <zejnils@gmail.com>
2021-03-18 18:37:15 +0100

    #49 Revert

--- examples/src/test/scala/za/co/absa/atum/HdfsInfoIntegrationSuite.scala
+++ examples/src/test/scala/za/co/absa/atum/HdfsInfoIntegrationSuite.scala
@@ -88,6 +88,7 @@ class HdfsInfoIntegrationSuite extends AnyFlatSpec with SparkTestBase with Match
           log.info(s"Checking $expectedPath to contain expected values")
 
           val infoControlMeasures =  eventually(timeout(scaled(10.seconds)), interval(scaled(2.seconds))) {
+            println("Trying path " + expectedPath)
             val infoContentJson = LocalFsTestUtils.readFileAsString(expectedPath)
             ControlMeasuresParser.fromJson(infoContentJson)
           }
