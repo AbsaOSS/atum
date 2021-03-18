5178d4bb6e35e6a2e68858712e8be36e2765e59c
Sasa Zejnilovic <zejnils@gmail.com>
2021-03-18 18:10:48 +0100

    #49 Test action

--- examples/src/main/scala/za/co/absa/atum/examples/SampleMeasurements1.scala
+++ examples/src/main/scala/za/co/absa/atum/examples/SampleMeasurements1.scala
@@ -50,11 +50,11 @@ object SampleMeasurements1 extends Eventually {
       .write.mode(SaveMode.Overwrite)
       .parquet("data/output/stage1_job_results")
 
-    spark.disableControlMeasuresTracking()
-
     eventually(timeout(scaled(10.seconds)), interval(scaled(500.millis))) {
       if (!Files.exists(Paths.get("data/output/stage1_job_results/_INFO")))
         throw new Exception("_INFO file not found at data/output/stage1_job_results")
     }
+
+    spark.disableControlMeasuresTracking()
   }
 }
