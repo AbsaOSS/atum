d81eaf2de58ab9dafa8a8f5d70a140fc1192a10b
Sasa Zejnilovic <zejnils@gmail.com>
2021-06-18 12:24:33 +0200

    #85 Unit Test tear apart

--- examples/src/test/scala/za/co/absa/atum/HdfsInfoIntegrationSuite.scala
+++ examples/src/test/scala/za/co/absa/atum/HdfsInfoIntegrationSuite.scala
@@ -15,24 +15,20 @@
 
 package za.co.absa.atum
 
-import java.nio.file.{Files, Paths}
-
 import org.apache.hadoop.fs.FileSystem
 import org.apache.log4j.LogManager
 import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.Eventually
-import org.scalatest.concurrent.PatienceConfiguration.Timeout
-import org.scalatest.flatspec.{AnyFlatSpec, AsyncFlatSpec}
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import org.specs2.matcher.Matchers.concurrentExecutionContext
+import za.co.absa.atum.AtumImplicits._
 import za.co.absa.atum.model.{Checkpoint, Measurement}
 import za.co.absa.atum.persistence.ControlMeasuresParser
 import za.co.absa.atum.utils.SparkTestBase
-import za.co.absa.atum.AtumImplicits._
 
-import scala.concurrent.duration.{Duration, DurationInt}
-import scala.concurrent.{Await, Future}
+import java.nio.file.{Files, Paths}
+import scala.concurrent.duration.DurationInt
 
 class HdfsInfoIntegrationSuite extends AnyFlatSpec with SparkTestBase with Matchers with BeforeAndAfterAll with Eventually {
 
@@ -59,48 +55,75 @@ class HdfsInfoIntegrationSuite extends AnyFlatSpec with SparkTestBase with Match
     }
   }
 
-  {
-    val outputPath = s"$tempDir/outputCheck1"
-    // implicit variant only writes to derived outputPath, explicit writes to both implicit derived path and the explicit one, too.
-    Seq(
-      ("implicit output _INFO path only", None, Seq(s"$outputPath/_INFO")),
-      ("implicit & explicit output _INFO path", Some(s"$outputPath/extra/_INFO2"), Seq(s"$outputPath/_INFO", s"$outputPath/extra/_INFO2"))
-    ).foreach { case (testCaseName, destinationOptInfoFilePath, expectedPaths) =>
+  val outputPath = s"$tempDir/outputCheck1"
 
-      "_INFO" should s"be written on spark.write ($testCaseName)" in {
-        val hadoopConfiguration = spark.sparkContext.hadoopConfiguration
-        implicit val fs: FileSystem = FileSystem.get(hadoopConfiguration)
+  "_INFO" should s"be written on spark.write (implicit output _INFO path only)" in {
+    val hadoopConfiguration = spark.sparkContext.hadoopConfiguration
+    implicit val fs: FileSystem = FileSystem.get(hadoopConfiguration)
 
-        // Initializing library to hook up to Apache Spark
-        spark.enableControlMeasuresTracking(sourceInfoFilePath = Some("data/input/wikidata.csv.info"), destinationInfoFilePath = destinationOptInfoFilePath)
-          .setControlMeasuresWorkflow("Job 1")
+    // Initializing library to hook up to Apache Spark
+    spark.enableControlMeasuresTracking(sourceInfoFilePath = Some("data/input/wikidata.csv.info"), destinationInfoFilePath = None)
+      .setControlMeasuresWorkflow("Job 1")
 
-        import spark.implicits._
-        val df1 = readSparkInputCsv(inputCsv)
-        df1.setCheckpoint("Checkpoint0")
-        val filteredDf1 = df1.filter($"total_response_size" > 1000)
-        filteredDf1.setCheckpoint("Checkpoint1") // stateful, do not need return value
-        writeSparkData(filteredDf1, outputPath) // implicit output _INFO file path is derived from this path passed to spark.write
+    import spark.implicits._
+    val df1 = readSparkInputCsv(inputCsv)
+    df1.setCheckpoint("Checkpoint0")
+    val filteredDf1 = df1.filter($"total_response_size" > 1000)
+    filteredDf1.setCheckpoint("Checkpoint1") // stateful, do not need return value
+    writeSparkData(filteredDf1, outputPath) // implicit output _INFO file path is derived from this path passed to spark.write
 
-        spark.disableControlMeasuresTracking()
+    spark.disableControlMeasuresTracking()
 
-        expectedPaths.foreach { expectedPath =>
-          log.info(s"Checking $expectedPath to contain expected values")
+    Seq(s"$outputPath/_INFO").foreach { expectedPath =>
+      log.info(s"Checking $expectedPath to contain expected values")
 
-          val infoControlMeasures =  eventually(timeout(scaled(10.seconds)), interval(scaled(2.seconds))) {
-            log.info(s"Reading $expectedPath")
-            val infoContentJson = LocalFsTestUtils.readFileAsString(expectedPath)
-            ControlMeasuresParser.fromJson(infoContentJson)
-          }
-
-          infoControlMeasures.checkpoints.map(_.name) shouldBe Seq("Source", "Raw", "Checkpoint0", "Checkpoint1")
-          val checkpoint0 = infoControlMeasures.checkpoints.collectFirst { case c: Checkpoint if c.name == "Checkpoint0" => c }.get
-          checkpoint0.controls should contain(Measurement("recordCount", "count", "*", "5000"))
-
-          val checkpoint1 = infoControlMeasures.checkpoints.collectFirst { case c: Checkpoint if c.name == "Checkpoint1" => c }.get
-          checkpoint1.controls should contain(Measurement("recordCount", "count", "*", "4964"))
-        }
+      val infoControlMeasures =  eventually(timeout(scaled(10.seconds)), interval(scaled(2.seconds))) {
+        log.info(s"Reading $expectedPath")
+        val infoContentJson = LocalFsTestUtils.readFileAsString(expectedPath)
+        ControlMeasuresParser.fromJson(infoContentJson)
       }
+
+      infoControlMeasures.checkpoints.map(_.name) shouldBe Seq("Source", "Raw", "Checkpoint0", "Checkpoint1")
+      val checkpoint0 = infoControlMeasures.checkpoints.collectFirst { case c: Checkpoint if c.name == "Checkpoint0" => c }.get
+      checkpoint0.controls should contain(Measurement("recordCount", "count", "*", "5000"))
+
+      val checkpoint1 = infoControlMeasures.checkpoints.collectFirst { case c: Checkpoint if c.name == "Checkpoint1" => c }.get
+      checkpoint1.controls should contain(Measurement("recordCount", "count", "*", "4964"))
+    }
+  }
+
+  "_INFO" should s"be written on spark.write (implicit & explicit output _INFO path)" in {
+    val hadoopConfiguration = spark.sparkContext.hadoopConfiguration
+    implicit val fs: FileSystem = FileSystem.get(hadoopConfiguration)
+
+    // Initializing library to hook up to Apache Spark
+    spark.enableControlMeasuresTracking(sourceInfoFilePath = Some("data/input/wikidata.csv.info"), destinationInfoFilePath = Some(s"$outputPath/extra/_INFO2"))
+      .setControlMeasuresWorkflow("Job 1")
+
+    import spark.implicits._
+    val df1 = readSparkInputCsv(inputCsv)
+    df1.setCheckpoint("Checkpoint0")
+    val filteredDf1 = df1.filter($"total_response_size" > 1000)
+    filteredDf1.setCheckpoint("Checkpoint1") // stateful, do not need return value
+    writeSparkData(filteredDf1, outputPath) // implicit output _INFO file path is derived from this path passed to spark.write
+
+    spark.disableControlMeasuresTracking()
+
+    Seq(s"$outputPath/_INFO", s"$outputPath/extra/_INFO2").foreach { expectedPath =>
+      log.info(s"Checking $expectedPath to contain expected values")
+
+      val infoControlMeasures =  eventually(timeout(scaled(10.seconds)), interval(scaled(2.seconds))) {
+        log.info(s"Reading $expectedPath")
+        val infoContentJson = LocalFsTestUtils.readFileAsString(expectedPath)
+        ControlMeasuresParser.fromJson(infoContentJson)
+      }
+
+      infoControlMeasures.checkpoints.map(_.name) shouldBe Seq("Source", "Raw", "Checkpoint0", "Checkpoint1")
+      val checkpoint0 = infoControlMeasures.checkpoints.collectFirst { case c: Checkpoint if c.name == "Checkpoint0" => c }.get
+      checkpoint0.controls should contain(Measurement("recordCount", "count", "*", "5000"))
+
+      val checkpoint1 = infoControlMeasures.checkpoints.collectFirst { case c: Checkpoint if c.name == "Checkpoint1" => c }.get
+      checkpoint1.controls should contain(Measurement("recordCount", "count", "*", "4964"))
     }
   }
 
