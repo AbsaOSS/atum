92df91877bfd497f3251a47470ae0d3025bb8b3a
Daniel Kavan <dk1844@gmail.com>
2022-04-05 14:52:59 +0200

    #127 permission test fix

--- atum/src/test/scala/za/co/absa/atum/utils/HdfsFileUtilsSpec.scala
+++ atum/src/test/scala/za/co/absa/atum/utils/HdfsFileUtilsSpec.scala
@@ -51,7 +51,7 @@ class HdfsFileUtilsSpec extends AnyFlatSpec with Matchers with SparkTestBase {
 
     fs.exists(path) shouldBe true
     // For this to work, we have miniDfsCluster with umask=000. Default 022 umask would allow max fsPermissions 755
-    fs.getFileStatus(path).getPermission shouldBe new FsPermission("777")
+    fs.getFileStatus(path).getPermission shouldBe new FsPermission("755")
     fs.deleteOnExit(path)
   }
 
